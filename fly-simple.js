7920b63d2bdd9fac30513da834a5cb36453555b7
harshiet <harsh.agarwal82@gmail.com>
2015-05-05 21:38:01 -0400

    1

--- fly-simple.js
+++ fly-simple.js
@@ -2,28 +2,51 @@ var arDrone = require('ar-drone');
 var client = arDrone.createClient();
 var fs = require('fs'), df = require('dateformat');
 try {
+	var rotateAndTakePicture = function(index) {
+		client.stop();
+		client.clockwise(.75);
+		client.back(.5);
+		setTimeout(function() {
+			client.stop();
+			setTimeout(function() {
+				client.animateLeds('blinkOrange', 5, 2);
+				client.getPngStream().once('data', function(data) {
+					console.log('taking picture');
+					var fileName = 'pano_' + index + '.png';
+					fs.writeFile(fileName, data, function(err) {
+						if (err) {
+							console.log(err);
+						} else {
+							console.log(fileName + ' Saved');
+						}
+					});
+					if (index == 4) {
+						client.land(function() {
+							console.log('landing');
+							process.exit(0);
+						})
+					} else {
+						rotateAndTakePicture(index + 1);
+					}
+				});
+			}, 2000);
+		}, 1000);
+	}
 	var takeoffCallBack = function() {
-		client.getPngStream().once('data', function(data) {
-			var fileName = 'pano_.png';
-			fs.writeFile(fileName, data, function(err) {
-				if (err) {
-					console.log(err);
-				} else {
-					console.log(fileName + ' Saved');
-				}
-			});
-			client.land(function() {
-				process.exit(0);
-			})
-		});
-
+		console.log('hovering');
+		// console.log('calibrating');
+		// client.calibrate(0);
+		// console.log('calibration done');
+		setTimeout(function() {
+			rotateAndTakePicture(1);
+		}, 1000);
 	}
 
 	client.ftrim()
 	setTimeout(function() {
+		console.log('taking off');
 		client.takeoff(takeoffCallBack);
-		client.stop();
-	}, 5000);
+	}, 2000);
 
 } catch (err) {
 	console.log(err);
