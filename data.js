676e27b84b740f337b744d7c9c345c115ffa2afe
harshiet <harsh.agarwal82@gmail.com>
2015-05-12 18:57:29 -0400

    1

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
 var arDrone = require('ar-drone');
-var client = arDrone.createClient();
+var client = arDrone.createClient({'ip': '192.168.1.12'});
 var pngStream = client.getPngStream();
 pngStream.on('data', console.log);
